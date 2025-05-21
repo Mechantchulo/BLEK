3271be9d6fa84d26c8e6076d9f3999baf9265b4d
Mechantchulo <chulomechant@gmail.com>
2025-05-21 17:15:04 +0300

    Add professional model

---
+++ app/models/professional.py
@@ -0,0 +1,12 @@
+from app import db
+
+class Professional(db.Model):
+    __tablename__ = 'professional'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    certification_file = db.Column(db.String(100), nullable=False)
+    portfolio_images = db.Column(db.JSON)
+    is_verified = db.Column(db.Boolean, default=False)
+    # Relationships
+    user = db.relationship("User", back_populates="professional")
